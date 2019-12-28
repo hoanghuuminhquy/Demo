a692016a1d258a3d64cc9b6671a21478ccbf6c03
hoanghuuminhquy <56187746+hoanghuuminhquy@users.noreply.github.com>
2019-12-28 17:43:53 +0700

    Add project files.

---
+++ QuanLyBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.Where(n => n.DaXoa == false).OrderByDescending(n => n.MaSP));
+        }
+
+        public ActionResult Chitietsp(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Nếu không thì truy xuất csdl lấy ra sản phẩm tương ứng id
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id && n.DaXoa == false);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+
+        public ActionResult Danhmucsp(int MaLoaiSP)
+        {
+            ViewBag.MaLoaiSP = MaLoaiSP;
+            return View(db.SanPhams.Where(n => n.DaXoa == false&&n.MaLoaiSP==MaLoaiSP));
+        }
+    }
+}
